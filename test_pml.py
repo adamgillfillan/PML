0a5f5ee51e96a1f7f9aa1268e99feb8a066c54ef
Adam Gillfillan <agillfi@ncsu.edu>
2014-11-17 17:45:25 -0500

    addded test case

--- test_pml.py
+++ test_pml.py
@@ -4,24 +4,30 @@ import pml
 
 
 class TestPML(unittest.TestCase):
+    """
+        Test the functionality of PML
+    """
     def setUp(self):
-        self.code = '''
-                <html>
-                <head>
-                    <title></title>
-                </head>
-                    <body>
-                        <h1>Hello There</h1>
-                        <p>
-                            This is an example of a pml file
-                        </p>
-                        <pml>
-                            def f():
-                                return "<h2>Good Bye</h2>"
+        self.code = '''<html>
+<head>
+    <title>This is a Title</title>
+</head>
+    <body>
+        <h1>Hello There</h1>
+        <p>
+            This is an example of a pml file
+        </p>
+        <pml>
+            def f():
+                return "<h2>Good Bye</h2>"
 
-                            pml = f()
-                        </pml>
-                    </body>
-                </html>
-                '''
+            pml = f()
+        </pml>
+    </body>
+</html>'''
+        self.filename = 'pml_sample.html'
+        self.f = open(self.filename, 'r')
+        self.pml = pml.PML(self.f)
 
+    def test_pml_print(self):
+        self.assertEquals(self.code, self.pml.print_file())
