58eb4f476a872ec1ea584f8932c603588e334df3
Adam Gillfillan <agillfi@ncsu.edu>
2014-11-17 21:19:28 -0500

    finished algorithm to format blocks of pml code

--- pml.py
+++ pml.py
@@ -13,18 +13,43 @@ class PML:
     """
 
     def __init__(self, file):
+        self.file = file
         self.code = file.read()
-        self.blocks = []
-        self.create_blocks()
+        # self.formatted_code = self.format()
+        self.blocks = self.create_blocks()
+        self.sub_blocks = self.format()
 
     def print_file(self):
         return self.code
+
+    def format(self):
+        """Formats self.blocks by removing unnecessary indents in the code."""
+        sub_blocks = []
+        for block in self.blocks:
+            sub_blocks.append(block.split('\n'))
+        new_blocks = []
+        for sub_block in sub_blocks:
+            count = 0
+            for element in sub_block[1:]:
+                if count == 0:
+                    new_blocks.append(element.lstrip())
+                    value = len(element) - len(element.lstrip())
+                    count = 1
+                else:
+                    new_value = len(element) - len(element.lstrip())
+                    value_2 = new_value - value
+                    new_blocks.append(element.replace(" "*new_value, " "*value_2))
+        new_blocks.insert(0, "")
+        # return "\n".join(new_blocks)
+        return new_blocks
 
     def create_blocks(self):
         """Creates blocks of python code and returns them in a list."""
         pattern = r"<pml>(.*?)</pml>"
         p = re.compile(pattern, re.DOTALL)
-        self.blocks = [x for x in re.findall(p, self.code)]
+        # TODO: Replace self.code with self.formatted_code
+        blocks = [x for x in re.findall(p, self.code)]
+        return blocks
 
         # Substitute with a 1, 2, etc.
         # pattern = r"(\s*<pml>.*?</pml>)"
@@ -38,9 +63,17 @@ def main():
     f = open(filename, 'r')
     pml = PML(f)
     #print(pml.print_file())
-    # for block in pml.blocks:
-    #     print(block)
+    #for block in pml.blocks:
+    #    print(block)
+
+    # TODO: Uncomment this
     print(pml.blocks)
+    print(pml.sub_blocks)
+    # for block in pml.blocks:
+    #     exec block
+
+    #print pml.code
+   # print pml.formatted_code
 
 if __name__ == "__main__":
     main()
